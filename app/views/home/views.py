1491c9bc2e1339032296c71382161ed82394a32a
colorsakura <2268136725@qq.com>
2018-09-19 00:31:00 -0400

    so

--- app/views/home/views.py
+++ app/views/home/views.py
@@ -1,7 +1,7 @@
 #coding:utf-8
 from . import home
-from flask import render_template
+from flask import render_template, url_for
 
 @home.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
